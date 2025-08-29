eef359ca122e98adf80ad284ce0329ef6598cda0
Abdulbasit <abdulbasitkayode@gmail.com>
2025-08-29 09:14:34 +0100

    Register Book model with custom admin display for title, author, and published year

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Book
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "published_year")
 # Register your models here.
-admin.site.register(Book)
-
+admin.site.register(Book, BookAdmin)
