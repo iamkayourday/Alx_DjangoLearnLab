0804758142a6def19119a21d5595f41c242e156f
Abdulbasit <abdulbasitkayode@gmail.com>
2025-08-29 09:16:46 +0100

    Fix typo in BookAdmin list_display and list_filter attributes

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "published_year")
-    list_filter = ("author", "published_year")
+    list_display = ("title", "author", "publication_year")
+    list_filter = ("author", "publication_year")
 # Register your models here.
 admin.site.register(Book, BookAdmin)
