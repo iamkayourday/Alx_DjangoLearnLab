307a2e8c4db3d3ef8ceb59eeeef51567574ae9e3
Abdulbasit <abdulbasitkayode@gmail.com>
2025-09-04 02:14:38 +0100

    Uncomment library books retrieval and librarian query examples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,9 +7,11 @@ from relationship_app.models import Book, Author, Library
 
 
 # List all books in a library.
-Library.objects.get(name=library_name).books.all()
 # library_books = Library.objects.get(name="library_name").books.all()
+# Library.objects.get(name=library_name).books.all()
+
 
 
 # Retrieve the librarian for a library.
-librarian = Library.objects.get(name="library_name").librarian
+# librarian = Library.objects.get(name="library_name").librarian
+# Librarian.objects.get(library=
