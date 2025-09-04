56c1333b97ca0b2faa77ab29add99170a3eeb765
Abdulbasit <abdulbasitkayode@gmail.com>
2025-09-04 02:08:46 +0100

    Fix variable name typos in query samples for author and library retrieval

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,12 +2,12 @@
 from os import name
 from relationship_app.models import Book, Author, Library
 
-author_books = Book.objects.filter(author_name="Author Name")
+author_books = Book.objects.filter(authorn_ame="author_name")
 
 
 # List all books in a library.
-library_books = Library.objects.get(name="Library Name").books.all()
+library_books = Library.objects.get(name="library_name").books.all()
 
 
 # Retrieve the librarian for a library.
-librarian = Library.objects.get(name="Library Name").librarian
+librarian = Library.objects.get(name="library_name").librarian
