3321214855f3431dad2547fd16e17b5cf908eb1d
Abdulbasit <abdulbasitkayode@gmail.com>
2025-09-05 21:17:37 +0100

    Update logout view to specify template name for consistency

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('books/', list_books, name='book_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', RegisterView.as_view(), name='register'),
+    # path('register/', views.register, name='register'), Function based view alternative
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
 ]
