18b4b8f3587cc60628b460c540ef1d3474582beb
Abdulbasit <abdulbasitkayode@gmail.com>
2025-09-22 17:41:01 +0100

    Add generics view to List, view detail, create, update and delete books

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 
 # Create your views here.
+
+# For retrieving all books.
+class ListView(ListAPIView):
+    model = Book.objects.all()
+    serializer_class = BookSerializer
+
+# For retrieving a single book by ID.
+class DetailView(RetrieveAPIView):
+    model = Book.objects.all()
+    serializer_class = BookSerializer
+
+# For adding a new book.
+class CreateView(CreateAPIView):
+    model = Book.objects.all()
+    serializer_class = BookSerializer
+
+# For modifying an existing book.
+class pdateView(UpdateAPIView):
+    model = Book.objects.all()
+    serializer_class = BookSerializer
+
+# For removing a book.
+class DeleteView(DestroyAPIView):
+    model = Book.objects.all()
+    serializer_class = BookSerializer
