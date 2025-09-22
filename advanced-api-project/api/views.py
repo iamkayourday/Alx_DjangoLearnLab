3a617c29a06668a4d13b4d451f38a7d6ea8dc86d
Abdulbasit <abdulbasitkayode@gmail.com>
2025-09-22 18:00:20 +0100

    Add permission classes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 
 # Create your views here.
@@ -9,23 +10,28 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView,
 class ListView(ListAPIView):
     model = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # For retrieving a single book by ID.
 class DetailView(RetrieveAPIView):
     model = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # For adding a new book.
 class CreateView(CreateAPIView):
     model = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 # For modifying an existing book.
 class UpdateView(UpdateAPIView):
     model = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 # For removing a book.
 class DeleteView(DestroyAPIView):
     model = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
