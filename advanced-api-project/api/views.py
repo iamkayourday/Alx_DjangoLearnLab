58f765b4bc2448b3bd6409be59e54e5ebb840eeb
Abdulbasit <abdulbasitkayode@gmail.com>
2025-09-22 18:35:42 +0100

    Add filtering to Listview

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,35 +3,47 @@ from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+# from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 # Create your views here.
 
 # For retrieving all books.
 class ListView(ListAPIView):
-    model = Book.objects.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [
+        # DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
+
 
 # For retrieving a single book by ID.
 class DetailView(RetrieveAPIView):
-    model = Book.objects.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 # For adding a new book.
 class CreateView(CreateAPIView):
-    model = Book.objects.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
 # For modifying an existing book.
 class UpdateView(UpdateAPIView):
-    model = Book.objects.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
 # For removing a book.
 class DeleteView(DestroyAPIView):
-    model = Book.objects.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
