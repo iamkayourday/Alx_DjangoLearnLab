3c038fa342ad724c749883b39f5bd174c4d13bf9
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-05 19:55:59 +0100

    Add post management URLs and views for update and delete functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, MyLoginView, MyLogoutView,profile, PostListView, PostCreateView
+from .views import RegisterView, MyLoginView, MyLogoutView,profile, PostListView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -7,5 +7,8 @@ urlpatterns = [
     path('logout/', MyLogoutView.as_view(), name='logout'),
     path('profile/', profile, name='profile'),
     path('posts/', PostListView.as_view(), name='posts'),
-    path('posts/new', PostCreateView.as_view(), name='create')
+    path('posts/new', PostCreateView.as_view(), name='create'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('post/<int:pk>/update/', PostDeleteView.as_view(), name='update'),
+
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 
 from django.views.generic import  ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 # Create your views here.
