fe60435a47d722ba15b73614f76bfb9549cba341
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-06 19:46:49 +0100

    Refactor CustomUserSerializer to use get_user_model for user creation

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,14 +1,22 @@
-from rest_framework.authtoken.models import Token
-from rest_framework import serializers
 from .models import CustomUser
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'date_of_birth', 'location']
         read_only_fields = ['followers']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)  # Create a token for the new user
         return user
+    
+
+
+
+
+
+# accounts/serializers.py doesn't contain: ["serializers.CharField()", "get_user_model().objects.create_user"]
