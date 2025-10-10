81fb2e122ef6f4bca25e660d9edb31a2d86ad19e
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-10 22:09:16 +0100

    Add following field to CustomUser model and update related migrations

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='user_following', blank=True)
     date_of_birth = models.DateField(blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='user_followers', blank=True)
