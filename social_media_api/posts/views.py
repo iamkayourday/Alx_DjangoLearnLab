91582c955ce3397eb233e8368a00edbc47cc6c25
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-10 22:05:12 +0100

    checker error

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from .models import Posts, Comments
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
-class PostViewSet(ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Posts.objects.all()
     # Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
-class CommentViewSet(ModelViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comments.objects.all()
     #  Comment.objects.all()
     serializer_class = CommentSerializer
