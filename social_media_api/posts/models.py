f5fc45f7dfe837576baaa7d56324ee4d2707f524
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-10 21:55:31 +0100

    Add initial models and migrations for Posts and Comments, and configure CustomUser as the user model

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    author = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comments(models.Model):
+    post = models.ForeignKey(Posts, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'accounts',
+    'posts',
     'rest_framework_simplejwt',
 ]
 
@@ -129,3 +130,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.CustomUser'
