94d606783ab9e1e9971ca1c42ad7078939103995
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-10 21:46:38 +0100

    Fix URL formatting in accounts app for consistency

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('register/', UserListCreateView.as_view(), name='user-list-create'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('profile/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-detail'),
+    path("register/", UserListCreateView.as_view(), name='user-list-create'),
+    path("login/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("token/refresh/", TokenRefreshView.as_view(), name='token_refresh'),
+    path("profile/<int:pk>/", UserRetrieveUpdateDestroyView.as_view(), name='user-detail'),
 ]
