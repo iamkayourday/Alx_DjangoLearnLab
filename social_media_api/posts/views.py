09e8549a8d22873dc83ec79228907c6cf6875194
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-10 22:19:57 +0100

    Checker requieremnt

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,24 +3,24 @@ from rest_framework import viewsets
 from .models import Posts, Comments
 from rest_framework.generics import ListAPIView
 from .serializers import PostSerializer, CommentSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 
 # Create your views here.
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Posts.objects.all()
     # Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comments.objects.all()
     #  Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
 class FeedAPIView(ListAPIView):
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
