7e9b50cb29dfe71a0b86f866f27e3a4e7f5f7c08
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-10 21:58:47 +0100

    include post url

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
+    path('api/', include('posts.urls')),
     
  
 ]
