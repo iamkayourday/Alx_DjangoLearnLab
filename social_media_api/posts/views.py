958e9fa1b895f9fca9aa56f83fce536b7724075c
Abdulbasit <abdulbasitkayode@gmail.com>
2025-10-10 22:03:56 +0100

    checker

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -7,10 +7,12 @@ from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 class PostViewSet(ModelViewSet):
     queryset = Posts.objects.all()
+    # Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
 class CommentViewSet(ModelViewSet):
     queryset = Comments.objects.all()
+    #  Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
